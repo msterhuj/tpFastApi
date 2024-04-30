f9e9a266201ad016a9d1741638df34fc7e65db80
msterhuj <gabin.lanore@gmail.com>
2024-04-30 20:03:59 +0200

    feat: move log api to router

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
-from typing import Sequence, Type, Dict, Any
+from typing import Sequence, Type
 from pydantic import BaseModel
 
 from fastapi import FastAPI, HTTPException
 from sqlmodel import Session, select
 from .models import User, Log, Severity, get_hashed_password
-from . import engine
+from . import engine, views
 from .database import create_db_and_tables
 
 app = FastAPI()
 
+app.include_router(views.log_router)
 
 @app.on_event("startup")
 def on_startup() -> None:
@@ -47,76 +48,3 @@ def create_user(user: UserInsert):
         session.add(user_db)
         session.commit()
         return {"id": user_db.id, "name": user_db.name}
-
-
-@app.get("/logs/")
-def read_logs() -> Sequence[Log]:
-    with Session(engine) as session:
-        logs = session.exec(select(Log)).all()
-        return logs
-
-
-@app.get("/logs/{severity}")
-def read_logs_by_severity(severity: Severity) -> Sequence[Log]:
-    with Session(engine) as session:
-        logs = session.exec(
-            select(Log)
-            .where(Log.severity == severity)
-            .order_by(Log.id.desc())
-        ).all()
-        return logs
-
-
-class LogInsert(BaseModel):
-    host: str
-    service: str
-    message: str
-    timestamp: int
-
-
-class LogUpdate(BaseModel):
-    host: str | None = None
-    service: str | None = None
-    message: str | None = None
-    timestamp: int | None = None
-
-
-@app.post("/logs/{severity}")
-def create_log(severity: Severity, log: LogInsert) -> Log:
-    with Session(engine) as session:
-        log = Log(
-            host=log.host,
-            service=log.service,
-            message=log.message,
-            severity=severity,
-            timestamp=log.timestamp,
-        )
-        session.add(log)
-        session.commit()
-        session.refresh(log)
-        return log
-
-
-@app.patch("/logs/{log_id}", response_model=Log)
-def update_log(log_id: int, log: LogUpdate) -> Type[Log] | None:
-    with Session(engine) as session:
-        db_log = session.get(Log, log_id)
-        if db_log is None:
-            raise HTTPException(status_code=404, detail="Log not found")
-        log_data = log.model_dump(exclude_unset=True)
-        db_log.sqlmodel_update(log_data)
-        session.add(db_log)
-        session.commit()
-        session.refresh(db_log)
-        return db_log
-
-
-@app.delete("/logs/{log_id}")
-def delete_log(log_id: int) -> dict[str, str]:
-    with Session(engine) as session:
-        db_log = session.get(Log, log_id)
-        if db_log is None:
-            raise HTTPException(status_code=404, detail="Log not found")
-        session.delete(db_log)
-        session.commit()
-        return {"message": "Log deleted"}
--- app/views/log.py
+++ app/views/log.py
@@ -1,12 +1,27 @@
-from typing import Sequence
+from typing import Sequence, Type
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 from sqlmodel import Session, select
 
-from app.models import Log
+from app.models import Log, Severity
 from app import engine
 
 router = APIRouter()
+
+
+class LogInsert(BaseModel):
+    host: str
+    service: str
+    message: str
+    timestamp: int
+
+
+class LogUpdate(BaseModel):
+    host: str | None = None
+    service: str | None = None
+    message: str | None = None
+    timestamp: int | None = None
 
 
 @router.get("/logs/", tags=["logs"])
@@ -14,3 +29,55 @@ def read_logs() -> Sequence[Log]:
     with Session(engine) as session:
         logs = session.exec(select(Log)).all()
         return logs
+
+
+@router.get("/logs/{severity}", tags=["logs"])
+def read_logs_by_severity(severity: Severity) -> Sequence[Log]:
+    with Session(engine) as session:
+        logs = session.exec(
+            select(Log)
+            .where(Log.severity == severity)
+            .order_by(Log.id.desc())
+        ).all()
+        return logs
+
+
+@router.post("/logs/{severity}", tags=["logs"])
+def create_log(severity: Severity, log: LogInsert) -> Log:
+    with Session(engine) as session:
+        log = Log(
+            host=log.host,
+            service=log.service,
+            message=log.message,
+            severity=severity,
+            timestamp=log.timestamp,
+        )
+        session.add(log)
+        session.commit()
+        session.refresh(log)
+        return log
+
+
+@router.patch("/logs/{log_id}", response_model=Log, tags=["logs"])
+def update_log(log_id: int, log: LogUpdate) -> Type[Log] | None:
+    with Session(engine) as session:
+        db_log = session.get(Log, log_id)
+        if db_log is None:
+            raise HTTPException(status_code=404, detail="Log not found")
+        log_data = log.model_dump(exclude_unset=True)
+        db_log.sqlmodel_update(log_data)
+        session.add(db_log)
+        session.commit()
+        session.refresh(db_log)
+        return db_log
+
+
+@router.delete("/logs/{log_id}", tags=["logs"])
+def delete_log(log_id: int) -> dict[str, str]:
+    with Session(engine) as session:
+        db_log = session.get(Log, log_id)
+        if db_log is None:
+            raise HTTPException(status_code=404, detail="Log not found")
+        session.delete(db_log)
+        session.commit()
+        return {"message": "Log deleted"}
